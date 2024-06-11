3cc05fb9b26c365b0d89b9041d00031cc6434069
Михаил Шатов <mshatov@inbox.ru>
2024-06-11 09:55:15 +0300

    add sentry

---
+++ task_1-sentry.py
@@ -0,0 +1,23 @@
+import sentry_sdk
+from flask import Flask
+sentry_sdk.init(
+    dsn="https://e9318f5b96e488a94524397dd5f3d69d@o4507406755102720.ingest.de.sentry.io/4507406758051922",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    1/0 # raises an error
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
